58781cd7c64b799a1a562da1d451a68e3180615b
rtkazan <126402804+rtkazan@users.noreply.github.com>
2023-04-15 11:26:36 +0300

    Add files via upload

---
+++ Task68.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите два положительных числа: m и n.");
+int m = InputInt("Введите m: ");
+int n = InputInt("Введите n: ");
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
