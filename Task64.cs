581d39fd95b0728315b51e19afa274ce62d3c649
rtkazan <126402804+rtkazan@users.noreply.github.com>
2023-04-15 11:15:54 +0300

    Add files via upload

---
+++ Task64.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+int n = InputInt("Введите положительное число: ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Вы ввели отрицательное число");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
