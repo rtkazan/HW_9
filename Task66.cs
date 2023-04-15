756825ef8dfcaf32933f0ec04d345cbc9999dbe0
rtkazan <126402804+rtkazan@users.noreply.github.com>
2023-04-15 11:24:06 +0300

    Add files via upload

---
+++ Task66.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+int m = InputInt("Введите m: ");
+int n = InputInt("Введите n: ");
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNaturalSum(m, n)}");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
